bbfbfd8efdadfdbd1bcdbb5571b7b95ef074089a
Derek Ely <103849541+derekely@users.noreply.github.com>
2024-01-10 10:38:25 -0700

    Add files via upload

---
+++ hello/hello/src/hello_app.erl
@@ -0,0 +1,29 @@
+%%%-------------------------------------------------------------------
+%% @doc hello public API
+%% @end
+%%%-------------------------------------------------------------------
+
+-module(hello_app).
+
+-behaviour(application).
+
+-export([start/2, stop/1]).
+
+start(_Type, _Args) ->
+        Dispatch = cowboy_router:compile([
+            {'_', [
+                {"/", default_page_h, []}
+            ]}
+        ]),
+        cowboy:start_clear(
+            my_http_listener,
+            [{port, 8080}],
+            #{env => #{dispatch => Dispatch}}
+        ),
+        hello_sup:start_link().
+stop(_State) ->
+    ok.                                                                                     
+
+%% internal functions
+
+%% internal functions
